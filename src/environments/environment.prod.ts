6bbb6f4fc23121378f2c29a9444f648f4b82dbe2
rluco <renatoluco@gmail.com>
2026-08-26 18:33:29 -0400

    [FIX] se agrega tambien a env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,7 @@ export const environment = {
 
   /* Ver la nota de environment.ts: es la misma URL, aparte para poder apuntar
      produccion a otra implementacion del Apps Script sin tocar el dev. */
-  sheetEndpoint:
-    'https://script.google.com/macros/s/AKfycbx9zR6LvacdNV_TE_PU5OycAFK8NX79PXkhiAMgx7lnoE55CV-41GipL5FC7rvc8_VROw/exec',
+  sheetEndpoint: 'https://app-sample-e8c098cfc70b.herokuapp.com/leaderboard',
 
   /* En produccion nunca se muestran participantes falsos. */
   useMockData: false,
